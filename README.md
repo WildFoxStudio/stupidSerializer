254d5727cf9654a57e2746666e586a554fa0a102
WildFoxStudio <kirichenko.stanislav@outlook.it>
2025-02-14 07:26:36 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,276 @@
 # stupidSerializer
-Just a stupid templated c++20 serializer
+
+Just a header only stupid templated c++20 serializer/deserializer.
+
+Disclaimer: Only std::vector is serializable because lack of time.
+
+# Usage example
+
+```cpp
+using namespace Fox;
+
+struct Vec3
+{
+	float x, y, z;
+
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::Value<Archive_T>("x", &x),
+			metaFrom::Value<Archive_T>("y", &y),
+			metaFrom::Value<Archive_T>("z", &z), };
+	}
+};
+
+struct metaVec3 : public meta<Vec3>
+{
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::Value<Archive_T>("Vec3", t)
+		};
+	}
+};
+```
+
+Now serialization
+
+```cpp
+    Vec3 vec{0,1,2};
+	CJsonArchive j{};//Defined elsewhere
+	metaVec3{&vec}.reflect(j).at(0)->Serialize(j);
+	std::cout << j.dump(4) << std::endl;
+```
+
+with output
+```json
+{
+    "Vec3": {
+            "x": 1.0,
+            "y": 2.0,
+            "z": 3.0
+            }
+}
+```
+
+
+Otherwise if you don't care about the struct/class name
+
+```cpp
+    Vec3 vec{0,1,2};
+	CJsonArchive j{};//Defined elsewhere
+    for (const auto& e : vec.reflect(j))
+	{
+		e->Serialize(j);
+	}
+	std::cout << j.dump(4) << std::endl;
+```
+With output:
+```json
+{    
+    "x": 0.0,
+    "y": 1.0,
+    "z": 2.0
+}
+```
+
+
+# Polymorphic classes
+
+```cpp
+struct Component
+{
+	int Value{};
+	Vec3 Pos{};
+
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::Value<Archive_T>("Value", &Value),
+			metaFrom::Value<Archive_T>("Pos", &Pos)
+		};
+	}
+};
+
+struct metaComponent : meta<Component>
+{
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::Value<Archive_T>("Component", t)
+		};
+	}
+};
+
+struct Derived : public Component
+{
+	double Friction{};
+	std::string Name;
+	std::vector<std::string> Vector;
+
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::Value<Archive_T>("Friction", &Friction),
+			metaFrom::Value<Archive_T>("Name", &Name),
+			metaFrom::Value<Archive_T>("Vector", &Vector),
+		};
+	}
+};
+
+struct metaDerived : meta<Derived>
+{
+	metaComponent base{ t };
+
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::DerivedClass<Archive_T>("Derived", &base, t)
+		};
+	}
+};
+
+struct MoreDerived : public Derived
+{
+	double Foozbar{};
+
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::Value<Archive_T>("Foozbar", &Foozbar),
+		};
+	}
+};
+
+struct metaMoreDerived : meta<MoreDerived>
+{
+	metaDerived base{ t };
+
+	template<typename Archive_T>
+	auto reflect(Archive_T archive) -> meta_list<Archive_T> {
+		return std::vector{
+			metaFrom::DerivedClass<Archive_T>("MoreDerived", &base, t)
+		};
+	}
+};
+```
+
+# Basic archive structure for nlohmann::json
+
+```cpp
+
+// Primary template: assume T is not serializable by default.
+template<typename T, typename = void>
+struct is_json_serializable : std::false_type {};
+
+// Specialization: SFINAE kicks in if the following expression is valid:
+//   to_json(json&, const T&)
+template<typename T>
+struct is_json_serializable<T, std::void_t<
+	decltype(to_json(std::declval<nlohmann::json&>(), std::declval<const T&>()))
+	>> : std::true_type {};
+
+// Helper variable template (C++14 and later)
+template<typename T>
+inline constexpr bool is_json_serializable_v = is_json_serializable<T>::value;
+
+class CJsonArchive {
+public:
+	CJsonArchive() = default;
+
+	explicit CJsonArchive(const nlohmann::json& j)
+		: m_json(j) {
+	}
+
+	explicit CJsonArchive(nlohmann::json&& j)
+		: m_json(std::move(j)) {
+	}
+
+	CJsonArchive(const CJsonArchive& other) {
+		m_json = other.m_json;
+	}
+
+	// Copy assignment operator.
+	CJsonArchive& operator=(const CJsonArchive& other) {
+		m_json = other.m_json;
+		return *this;
+	}
+
+	CJsonArchive& operator=(const nlohmann::json& json) {
+		m_json = json;
+		return *this;
+	}
+
+	// Checks if a key exists in the underlying JSON.
+	bool contains(const std::string& key) const {
+		return m_json.contains(key);
+	}
+
+	// Templated getter that uses nlohmann::json::get_to.
+	// Returns true if the key exists and the conversion succeeds.
+	template<typename T, typename std::enable_if_t<is_json_serializable_v<T>, bool> = true>
+	bool get(const std::string& key, T& outValue) const {
+		//static_assert(!utils::is_std_vector<T>::value);
+
+		if (!contains(key))
+			return false;
+		try {
+			m_json.at(key).get_to(outValue);
+			return true;
+		}
+		catch (const nlohmann::json::exception&) {
+			return false;
+		}
+	}
+
+	template<typename T, typename std::enable_if_t<std::is_same_v<T, CJsonArchive>, bool> = true>
+	bool get(const std::string& key, CJsonArchive& outValue) const {
+		if (!contains(key))
+			return false;
+		try {
+			nlohmann::json out{};
+			static_assert(!utils::is_std_vector<T>::value);
+			static_assert(!std::is_same_v<T, std::string>);
+			m_json.at(key).get_to(out);
+			outValue = out;
+			return true;
+		}
+		catch (const nlohmann::json::exception&) {
+			return false;
+		}
+	}
+
+
+	// Templated setter that assigns a value to a key.
+	template<typename T>
+	void set(const std::string& key, const T& value) {
+		m_json[key] = value;
+	}
+
+	// Templated setter that assigns a value to a key.
+	template<>
+	void set(const std::string& key, const CJsonArchive& value) {
+		m_json[key] = value.m_json;
+	}
+
+	// operator[] returns a new wrapper that references the sub-object for the given key.
+	// This allows chaining calls like: wrapper["key1"]["key2"].set(...);
+	CJsonArchive operator[](const std::string& key)const {
+		return CJsonArchive(m_json[key]);
+	}
+
+	inline std::string dump(const int i)const { return m_json.dump(i); }
+
+private:
+
+	nlohmann::json m_json;
+};
+```
+
+# Contribution
+
+Feel free to create a pull request.
+
+
+# Credits
+If you intend to use this lib give credit please. 
---
+++ include/stupidSerializer.h
@@ -0,0 +1,313 @@
+// //////////////////////////////////////////////////////////////////////////////////////////
+// FILE: stupidSerializer.h
+// 
+// AUTHOR: Kirichenko Stanislav
+// 
+// DATE: 14 feb 2025
+// 
+// DESCRIPTION: Stupid simple c++20 templated serialization with abstract concept for archive class.
+// 
+// LICENSE: BSD-2
+// Copyright (c) 2025, Kirichenko Stanislav
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions, and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions, and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#pragma once
+
+#include <vector>
+#include <type_traits>
+#include <tuple>
+#include <utility>
+#include <functional>
+#include <cassert>
+
+namespace Fox
+{
+	/**
+	 * /brief Base interface for serialization.
+	 */
+	template<typename Archive_T>
+	struct IWrap
+	{
+		inline static constexpr char INHERIT_TOKEN[]{ "%inherit_from%" };
+		virtual std::string Name()const = 0;
+		virtual void Serialize(std::reference_wrapper<Archive_T> j) const = 0;
+		virtual void Deserialize(std::reference_wrapper<const Archive_T> j) = 0;
+	};
+
+	template<typename Archive_T>
+	using metaptr = std::shared_ptr<IWrap<Archive_T>>;
+	template<typename Archive_T>
+	using meta_list = std::vector<metaptr<Archive_T>>;
+
+	namespace utils
+	{
+		/**
+		 * /brief Check that reflect() returns a list of reflectable elements.
+		 */
+		template <typename T, typename Archive_T>
+		concept Reflectable = requires(T & t, Archive_T archive) {
+			{ t.template reflect<Archive_T>(archive) }  -> std::same_as<meta_list<Archive_T>>;
+		};
+
+		/**
+		 * /brief Checks if has post load method.
+		 */
+		template <typename T>
+		concept HasPostLoad = requires(T & t) {
+			{ t.postLoad() } -> std::same_as<void>;
+		};
+
+		/**
+		 * /brief The archive class concept.
+		 */
+		template<typename W>
+		concept ArchiveConcept = requires(W w, const W cw, std::string key, int testValue, int outValue) {
+			// Check that contains exists and returns a bool.
+			{ w.contains(key) } -> std::convertible_to<bool>;
+			{ cw.contains(key) } -> std::convertible_to<bool>;
+
+			// Check that get works for an int.
+			{ w.get(key, outValue) } -> std::convertible_to<bool>;
+
+			// Check that set works for an int.
+			{ w.set(key, testValue) } -> std::same_as<void>;
+
+			// Check that operator[] returns a new wrapper of the same type.
+			{ w[key] } -> std::same_as<W>;
+			{ cw[key] } -> std::same_as<W>;
+		};
+
+
+		template<typename T, typename Wrapper>
+		concept Archivable = requires(Wrapper w, const T & t, const std::string key, T temp, T outValue) {
+			{ w.get(key, outValue) } -> std::convertible_to<bool>;
+			{ w.set(key, t) } -> std::same_as<void>;
+		};
+
+		template<typename T>
+		struct is_std_vector : std::false_type
+		{
+		};
+
+		template<typename... Args>
+		struct is_std_vector<std::vector<Args...>> : std::true_type
+		{
+		};
+	}
+
+	/**
+	 * /brief Reflection struct to reflect the class name and recursively othere members.
+	 * /tparam T The class type.
+	 * /tparam Archive_T The archive type.
+	 */
+	template<typename T>
+	struct meta
+	{
+		//static_assert(utils::Reflectable<T>);
+		T* const t;
+	};
+
+	/**
+	 * /brief Templated type erasure wrapper for serialization.
+	 * /tparam T The type to be serialized.
+	 * /tparam Archive_T The archive used to serialize.
+	 */
+	template<typename T, typename Archive_T>
+	struct CWrap : public IWrap<Archive_T>
+	{
+		const std::string _name;
+		T* const _t;
+		CWrap(std::string name, T* const t) : _name(name), _t(t) { assert(_t); };
+		std::string Name()const override { return _name; }
+		virtual void Serialize(std::reference_wrapper<Archive_T> j) const override {
+			using namespace utils;
+
+			auto& archive{ j.get() };
+			if constexpr (Reflectable<T, Archive_T>)
+			{
+				//Recursive serialization
+				Archive_T k{};
+				for (const auto& e : _t->reflect(j.get()))
+				{
+					e->Serialize(k);
+				}
+
+				archive.set(_name, k);
+			}
+			else
+				if constexpr (std::is_trivial<T>::value || utils::Archivable<T, Archive_T>)
+				{
+					const auto& ref{ *_t };
+					archive.set(_name, ref);
+				}
+				else
+					if constexpr (is_std_vector<T>::value)
+					{
+						using elementType = typename T::value_type;
+						static_assert((std::is_trivial<T>::value || utils::Archivable<T, Archive_T>));
+
+						const std::vector<elementType>& vec{ *_t };
+
+						// Recursive serialization
+						std::vector<Archive_T> k{};
+
+						for (auto it{ vec.cbegin() }; it != vec.cend(); it++)
+						{
+							const elementType* const data{ &(*it) };
+
+							const CWrap e("element", data);
+
+							Archive_T j{};
+							e.Serialize(j);
+							k.push_back(j);
+						}
+
+						archive.set(_name, k);
+					}
+					else
+					{
+						static_assert(false, "Unsupported type");
+					}
+
+		};
+
+		virtual void Deserialize(std::reference_wrapper<const Archive_T> j) override
+		{
+			using namespace utils;
+
+			const auto& archive{ j.get() };
+
+			if constexpr (Reflectable<T, Archive_T>)
+			{
+				if (!archive.contains(_name))
+					throw std::runtime_error("Bad json.");
+				const auto inner{ archive[_name] };
+
+				const auto members{ _t->reflect(archive) };
+				for (const auto& e : members)
+				{
+					const auto name{ e->Name() };
+					if (!inner.contains(name))
+						throw std::runtime_error("Bad json.");
+
+					e->Deserialize(inner);
+				}
+			}
+			else
+				if constexpr (std::is_trivial<T>::value || utils::Archivable<T, Archive_T>)
+				{
+					if (!archive.contains(_name))
+						throw std::runtime_error("Bad json.");
+
+					auto& ref{ *_t };
+					archive.get(_name, ref);
+				}
+				else
+					if constexpr (is_std_vector<T>::value)
+					{
+						using elementType = typename T::value_type;
+						static_assert((std::is_trivial<T>::value || utils::Archivable<T, Archive_T>));
+
+						std::vector<elementType>& vec{ *_t };
+
+						if (!archive.contains(_name))
+							throw std::runtime_error("Bad json.");
+
+						// Recursive serialization
+						std::vector<Archive_T> k{};
+						archive.get(_name, k);
+						vec.resize(k.size());
+
+						for (auto it{ k.cbegin() }; it != k.cend(); it++)
+						{
+							elementType* const data{ &(vec.at(std::distance(k.cbegin(), it))) };
+							CWrap e("element", data);
+							data = e.Deserialize(*it);
+						}
+					}
+					else
+					{
+						static_assert(false, "Unsupported type");
+					}
+
+			if constexpr (HasPostLoad<T>)
+			{
+				_t->postLoad();
+			}
+		}
+	};
+
+	/**
+	 * /brief Templated type erasure wrapper for serialization of a derived class.
+	 * /tparam T
+	 */
+	template<typename T, typename Archive_T>
+	struct CWrapPolymorphic : public CWrap<T, Archive_T>
+	{
+		metaptr<Archive_T> _b;
+
+		CWrapPolymorphic(std::string name, metaptr<Archive_T> base, T* const t) : CWrap<T, Archive_T>(name, t), _b(base) {};
+		void Serialize(std::reference_wrapper<Archive_T> j) const override {
+			auto& archive{ j.get() };
+			CWrap<T, Archive_T>::Serialize(archive);
+			// Inject the base class
+			auto entry{ archive[CWrap<T, Archive_T>::Name()] };
+			_b->Serialize(entry);
+			// Apply changes
+			archive.set(CWrap<T, Archive_T>::Name(), entry);
+		};
+		void Deserialize(std::reference_wrapper<const Archive_T> j) override
+		{
+			const auto& archive{ j.get() };
+			CWrap<T, Archive_T>::Deserialize(archive);
+
+			const auto inner{ archive[CWrap<T, Archive_T>::_name] };
+
+			if (!inner.contains(IWrap<Archive_T>::INHERIT_TOKEN))
+				throw std::runtime_error("Bad json.");
+
+			// Inject the base class
+			_b->Deserialize(std::ref(inner));
+		}
+	};
+
+	/**
+	 * /brief Helper to wrap primitives or reflected classes.
+	 */
+	namespace metaFrom
+	{
+		template<typename Archive_T, typename T>
+		inline static metaptr<Archive_T> BaseClass(T* t) { return metaptr<Archive_T>{ new CWrap<T, Archive_T>(IWrap<Archive_T>::INHERIT_TOKEN, t) }; }
+
+		template<typename Archive_T, typename T>
+		inline static metaptr<Archive_T>  Value(std::string name, T* const t) { return metaptr<Archive_T>{ new CWrap<T, Archive_T>(name, t) }; }
+
+		template<typename Archive_T, typename TBase, typename T>
+		inline static metaptr<Archive_T> DerivedClass(std::string name, TBase* base, T* t) {
+			auto baseClass{ metaFrom::BaseClass<Archive_T>(base) };
+			return metaptr<Archive_T>{ new CWrapPolymorphic<T, Archive_T>(name, baseClass, t) };
+		}
+	};
+}
